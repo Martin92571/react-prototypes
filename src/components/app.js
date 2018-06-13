1fc2927e281c2551238e7237d160122387684dd0
Martin92571 <zegrizo@gmail.com>
2018-06-13 00:40:38 -0700

    functional-components -MArtin Rizo

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
-
+import React from 'react';
+import Table from './table';
+const student=[{student:"MArtin",course:"react",grade:"87"},{student:"Jaylen",course:"brat",grade:"95"},
+{student:"Leo",course:"preschool",grade:"97"}]
+export default ()=>{
+    return (<div className="container">
+     <h1>Student Grade Table</h1>
+     <Table data={student}/>
+    </div>
+    )}
 
